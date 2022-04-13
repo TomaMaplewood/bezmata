869eb21f06e8ffe45784e805db79ad88a7651fd4
Ivan Roudakov <roudakov.ivan@gmail.com>
2022-04-13 16:32:18 +0300

    Handling open-file-exception

--- code/main.py
+++ code/main.py
@@ -11,7 +11,13 @@ if __name__=="__main__":
     #It is temporary measure
 
     document_name = str(os.getenv("DOC_PATH"))
-    docx = Document(document_name)
+    try:
+        docx = Document(document_name)
+
+    except Exception as e:
+        print("Can not open file because of", str(e))
+        raise e
+
     for para in docx.paragraphs:
         word_finder.find_in_line(para.text)
     print(word_finder.get_list_results())
