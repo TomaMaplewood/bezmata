f0bc00551a2c3e2111d6d02d50d78def2271f0fd
Ivan Roudakov <roudakov.ivan@gmail.com>
2022-04-13 16:18:09 +0300

    Added basic word finder algorythm

--- code/main.py
+++ code/main.py
@@ -1,10 +1,16 @@
+import os
+
 from docx import Document
 from word_finder import WordFinder
 
 
 if __name__=="__main__":
     word_finder = WordFinder()
-    document_name = "C:\\Users\\Ivan\\Documents\\Начало работы.docx"
+
+    #Recuired to have environment variable DOC_PATH where you can define path to document
+    #It is temporary measure
+
+    document_name = str(os.getenv("DOC_PATH"))
     docx = Document(document_name)
     for para in docx.paragraphs:
         word_finder.find_in_line(para.text)
